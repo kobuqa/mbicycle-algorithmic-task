2bf68dbb53303f96f02e4e42fe3228bf57cd15b1
ogurez <ogurezslava@gmail.com>
2023-05-25 18:01:58 +0100

    Solved tasks

--- index.js
+++ index.js
@@ -11,7 +11,20 @@
  *     getNumPairs([2,3,4,5], 5, 7); // Return 4;
  */
 function getNumPairs(arr, l, r) {
-  // your implementation here
+  const visited = {}
+  const pairs = {}
+  let res = 0
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = l; j <= r; j++) {
+      if (visited[j - arr[i]] && !pairs[`${j-arr[i]}${arr[i]}`] && !pairs[`${arr[i]}${j-arr[i]}`]) {
+        res++
+        pairs[`${j-arr[i]}${arr[i]}`] = 1
+      }
+    }
+    visited[arr[i]] = 1
+  }
+
+  return res
 }
 
 module.exports = getNumPairs;
